3354ef86be2c1394532ba400b757cd8818be8411
AndreyAlth <andreyalth@gmail.com>
2023-08-29 17:48:28 -0600

    agregar titulo

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
     return (
         <div>
-            <h1 className="text-4xl">Amazing charts</h1>
+            <h1 className="text-4xl">Amazing charts template</h1>
         </div>
     )
 }
