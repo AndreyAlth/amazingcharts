7430827de3ecd2c319094e0be2328937f5e53d91
AndreyAlth <andreyalth@gmail.com>
2023-09-12 17:55:02 -0600

    agregar callback para google

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,16 +1,24 @@
-import NextAuth from "next-auth"
+import NextAuth from 'next-auth'
 // import GithubProvider from "next-auth/providers/github"
-import GoogleProvider from "next-auth/providers/google";
+import GoogleProvider from 'next-auth/providers/google'
 
 export const authOptions = {
-  // Configure one or more authentication providers
-  site: 'www.google.com',
-  providers: [
-    GoogleProvider({
-        clientId: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET
-      }),
-  ],
+    // Configure one or more authentication providers
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        }),
+    ],
+    callbacks: {
+        async signIn({ account, profile }) {
+            if (account.provider === 'google') {
+                // return profile.email_verified && profile.email.endsWith('@example.com')
+                console.log({account, profile})
+            }
+            return true // Do different verification for other providers that don't have `email_verified`
+        },
+    },
 }
 
 export default NextAuth(authOptions)
