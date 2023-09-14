acf2d2620950f63cb8f66aebc5bfc3d3f7f813e9
AndreyAlth <andreyalth@gmail.com>
2023-09-13 22:44:47 -0600

    agregar queries para crear usuarios y clientes

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,6 +1,8 @@
 import NextAuth from 'next-auth'
 // import GithubProvider from "next-auth/providers/github"
 import GoogleProvider from 'next-auth/providers/google'
+import { create_user } from '@/backend/db/user'
+import { create_client } from '@/backend/db/client'
 
 export const authOptions = {
     // Configure one or more authentication providers
@@ -14,7 +16,19 @@ export const authOptions = {
         async signIn({ account, profile }) {
             if (account.provider === 'google') {
                 // return profile.email_verified && profile.email.endsWith('@example.com')
-                console.log({account, profile})
+                try {
+                    const name = profile.name
+                    const client = await create_client({name})
+                    try {
+                        const [client_id, name, last_name, email, email_verified, profile_img] = [client.id]
+                        const user = create_client({})
+                    } catch (error) {
+                        
+                    }
+                    console.log(client)
+                } catch (error) {
+                    console.log(error);
+                }
             }
             return true // Do different verification for other providers that don't have `email_verified`
         },
--- src/pages/api/auth_jwt/sign_up.js
+++ src/pages/api/auth_jwt/sign_up.js
@@ -1,4 +1,4 @@
-import { get_user } from '@/backend/auth/database'
+import { get_user } from '@/backend/db/user'
 
 export default async function GET(req, res) {
     const user = await get_user()
