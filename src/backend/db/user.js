39dc4bfe6a6d92e29b3c81d8f72e86f48dd7ab58
AndreyAlth <andreyalth@gmail.com>
2023-09-15 00:39:54 -0600

    agregar validaciones de login

--- src/backend/db/user.js
+++ src/backend/db/user.js
@@ -5,6 +5,13 @@ export async function create_user({ client_id, name, last_name, email, email_ver
     const res = await conection.query(text, [client_id, name, last_name, email, email_verified, profile_img])
     return res.rowCount
 }
+
+export async function get_user_by_email(email) {
+    const text = `select * from users where email = $1`
+    const res = await conection.query(text, [email])
+    return res.rowCount
+}
+
 export async function get_user(user_id) {
     const text = `select * from users where id = $1`
     const res = await conection.query(text, [user_id])
--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,7 +1,7 @@
 import NextAuth from 'next-auth'
 // import GithubProvider from "next-auth/providers/github"
 import GoogleProvider from 'next-auth/providers/google'
-import { create_user } from '@/backend/db/user'
+import { create_user, get_user_by_email } from '@/backend/db/user'
 import { create_client } from '@/backend/db/client'
 
 export const authOptions = {
@@ -15,26 +15,30 @@ export const authOptions = {
     callbacks: {
         async signIn({ account, profile }) {
             if (account.provider === 'google') {
-                try {
-                    const name = profile.name
-                    const client = await create_client({ name })
+                const existed_user = await get_user_by_email(profile.email)
+                if (!existed_user) {
                     try {
-                        const [client_id, name, last_name, email, email_verified, profile_img] = [
-                            client.id,
-                            profile.given_name,
-                            profile.family_name,
-                            profile.email,
-                            profile.email_verified,
-                            profile.picture,
-                        ]
-                        const user = await create_user({ client_id, name, last_name, email, email_verified, profile_img })
-                        return user
+                        const name = profile.name
+                        const client = await create_client({ name })
+                        try {
+                            const [client_id, name, last_name, email, email_verified, profile_img] = [
+                                client.id,
+                                profile.given_name,
+                                profile.family_name,
+                                profile.email,
+                                profile.email_verified,
+                                profile.picture,
+                            ]
+                            const user = await create_user({ client_id, name, last_name, email, email_verified, profile_img })
+                            return user
+                        } catch (error) {
+                            console.log(error)
+                        }
                     } catch (error) {
                         console.log(error)
                     }
-                } catch (error) {
-                    console.log(error)
                 }
+                
             }
             return true // Do different verification for other providers that don't have `email_verified`
         },
