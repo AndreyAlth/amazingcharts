6b88126d509b92e352c4099a0892888a008cb965
andreyalth <andreyalth@gmail.com>
2023-09-26 15:16:00 -0600

    cambiar nombre de seccion

--- src/partials/client/newClient/NewClientContainer.jsx
+++ src/partials/client/newClient/NewClientContainer.jsx
@@ -16,11 +16,11 @@ function NewClientContainer() {
             <ul className="flex space-x-2 rtl:space-x-reverse">
                 <li>
                     <Link href="#" className="text-primary hover:underline">
-                        Users
+                        Comapny
                     </Link>
                 </li>
                 <li className="before:content-['/'] ltr:before:mr-2 rtl:before:ml-2">
-                    <span>Account Settings</span>
+                    <span>Create Company</span>
                 </li>
             </ul>
             <div className="pt-5">
