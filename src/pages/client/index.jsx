9cd38a52480c598fb4722743d85e4610de20a0a5
andreyalth <andreyalth@gmail.com>
2023-09-26 15:32:43 -0600

    agregar parcial de perfil al index

--- src/pages/client/index.jsx
+++ src/pages/client/index.jsx
@@ -2,16 +2,13 @@ import React, { useState } from 'react'
 import { useSession } from 'next-auth/react'
 import NewClientContainer from '@/partials/client/newClient/NewClientStart'
 import ClientProfile from '@/partials/client/Profile/ClientProfile'
-// import NewClientHome from '@/partials/client/newClient/NewClientHome'
 
 function Dashboard() {
     const { data: session } = useSession()
     return (
         <div>
             {
-                // session?.user?.client_id ? <div>Sí hay cliente</div> : <newClientContainer/>
-                // <NewClientContainer/>
-                <ClientProfile />
+                session?.user?.client_id ? <ClientProfile /> : <NewClientContainer/>
             }
         </div>
     )
