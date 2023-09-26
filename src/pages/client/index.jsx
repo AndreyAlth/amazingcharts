23d2658b4bfa74e84b2e14d6c8e41ecfd08d2c6a
andreyalth <andreyalth@gmail.com>
2023-09-26 15:27:15 -0600

    agregar parcial para perfil del cliente

--- src/pages/client/index.jsx
+++ src/pages/client/index.jsx
@@ -1,18 +1,20 @@
 import React, { useState } from 'react'
 import { useSession } from 'next-auth/react'
 import NewClientContainer from '@/partials/client/newClient/NewClientStart'
+import ClientProfile from '@/partials/client/Profile/ClientProfile'
 // import NewClientHome from '@/partials/client/newClient/NewClientHome'
 
 function Dashboard() {
-  const { data: session } = useSession()
-  return (
-    <div>
-      {
-        // session?.user?.client_id ? <div>Sí hay cliente</div> : <newClientContainer/>
-        <NewClientContainer/>
-      }
-    </div>
-  )
+    const { data: session } = useSession()
+    return (
+        <div>
+            {
+                // session?.user?.client_id ? <div>Sí hay cliente</div> : <newClientContainer/>
+                // <NewClientContainer/>
+                <ClientProfile />
+            }
+        </div>
+    )
 }
 
 export default Dashboard
