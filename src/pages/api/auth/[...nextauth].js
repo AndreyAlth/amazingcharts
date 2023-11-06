b165b0ba4d8c511848890280fdb092a4136cc320
andreyalth <andreyalth@gmail.com>
2023-11-05 21:16:56 -0600

    agregar validacion por credenciales

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -2,6 +2,7 @@ import NextAuth from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
 import { create_user, get_user_by_email } from '@/backend/db/user'
 import CredentialsProvider from 'next-auth/providers/credentials'
+import { hashPass } from '@/backend/user/password'
 
 let user_id = null
 let client_id = null
@@ -15,35 +16,39 @@ export const authOptions = {
         }),
         CredentialsProvider({
             // The name to display on the sign in form (e.g. 'Sign in with...')
-            name: 'Credentials',
+            name: 'Email and password',
             // The credentials is used to generate a suitable form on the sign in page.
             // You can specify whatever fields you are expecting to be submitted.
             // e.g. domain, username, password, 2FA token, etc.
             // You can pass any HTML attribute to the <input> tag through the object.
             credentials: {
-                username: { label: 'Username', type: 'text', placeholder: 'jsmith' },
+                // username: { label: 'Username', type: 'text', placeholder: 'jsmith' },
+                email: { label: 'Email', type: 'email', placeholder: 'email' },
                 password: { label: 'Password', type: 'password' },
             },
             async authorize(credentials, req) {
+                // const a = await hashPass(credentials.password)
+                // console.log(a)
+                return null
                 // You need to provide your own logic here that takes the credentials
                 // submitted and returns either a object representing a user or value
                 // that is false/null if the credentials are invalid.
                 // e.g. return { id: 1, name: 'J Smith', email: 'jsmith@example.com' }
                 // You can also use the `req` object to obtain additional parameters
                 // (i.e., the request IP address)
-                const res = await fetch('/your/endpoint', {
-                    method: 'POST',
-                    body: JSON.stringify(credentials),
-                    headers: { 'Content-Type': 'application/json' },
-                })
-                const user = await res.json()
+                // const res = await fetch('/your/endpoint', {
+                //     method: 'POST',
+                //     body: JSON.stringify(credentials),
+                //     headers: { 'Content-Type': 'application/json' },
+                // })
+                // const user = await res.json()
 
                 // If no error and we have user data, return it
-                if (res.ok && user) {
-                    return user
-                }
+                // if (res.ok && user) {
+                //     return user
+                // }
                 // Return null if user data could not be retrieved
-                return null
+                // return null
             },
         }),
     ],
